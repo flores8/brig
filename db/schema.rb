27a656bf6e39112ecf63b3b7e0747073aeea7687
flores8 <flores.lauralee@gmail.com>
2015-02-16 14:08:26 -0700

    renamed database files

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150216182815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "memories", force: true do |t|
     t.text     "post"
     t.string   "name"
