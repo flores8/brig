2837d01ad24d0384d8c3c8233445f70587d87b15
flores8 <flores.lauralee@gmail.com>
2015-02-22 09:07:37 -0700

    added custom styling to private form

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216182815) do
+ActiveRecord::Schema.define(version: 20150222155652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "memories", force: true do |t|
     t.text     "post"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "private_memories", force: true do |t|
+    t.text     "body"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
