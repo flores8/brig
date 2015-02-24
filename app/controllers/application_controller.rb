cb180af3b58ceaa8c56fb0b36ce57c309e1bffb5
flores8 <flores.lauralee@gmail.com>
2015-02-24 13:01:56 -0700

    updated pundit error message to a custom message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
-  protect_from_forgery with: :exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   alias_method :current_user, :current_admin
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-  	redirect_to root_url, alert: exception.message
+  # rescue_from Pundit::NotAuthorizedError do |exception|
+  # 	redirect_to root_url, alert: exception.message
+  # end
+
+  def user_not_authorized
+  	flash[:error] = "I'm sorry you're are not authorized to perform this action."
+  	redirect_to(request.referrer || root_path)
   end
 end
